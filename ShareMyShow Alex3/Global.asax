08beb5ef394ca86041b429e243eaef0334961374
Alex Shlimovich <Shlimovich@gmail>
2015-01-26 16:25:05 +0200

    Add Basic Files

---
+++ ShareMyShow Alex3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShareMyShow_Alex3.MvcApplication" Language="C#" %>
---
+++ ShareMyShow Alex3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShareMyShow_Alex3.Startup))]
+namespace ShareMyShow_Alex3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
